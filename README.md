b72a324d8ec177cf5cb75403d567277531608e86
Gudleik Rasch <gudleik@gmail.com>
2021-03-13 15:37:19 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
-# copy-previous-s3
+# Pulumi stack name
 
-# Example usage
+Github action to determine pulumi stack name. If the action is based on a pull request, the stack name becomes `pr-<Pull request number>` (ie `pr-1234`).
+Otherwise, the pulumi stack name becomes the name of the branch.
 
-```yaml
-jobs:
-  example:
-    steps:
-      - name: copy s3 from previous commit
-        uses: opronto/copy-previous-s3
-        id: prime
-        with:
-          component: foo
-          s3Bucket: pronto-build
-        env:
-          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-```
+Notes:
+
+- Slashes in branch names are replaced with `-`. Example `fix/my-Branch` becomes `fix-my-branch`
--- index.js
+++ index.js
@@ -1,67 +1,12 @@
-const AWS = require('aws-sdk')
-const exec = require('@actions/exec')
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const component = core.getInput('component')
-const Bucket = core.getInput('s3Bucket')
-const maxCount = Number(core.getInput('maxCount')) || 20
+const branch = github.context.ref
+  ?.replace(/^refs\/heads\//, '')
+  .replace(/\//, '-')
+  .toLowerCase()
 
-const s3 = new AWS.S3()
+const prNumber = github.context.payload.pull_request?.number
 
-async function getGitShas() {
-  let output = ''
-  await exec.exec('git', ['rev-list', 'HEAD', '--max-count', maxCount], {
-    silent: true,
-    listeners: {
-      stdout: (data) => {
-        output += data.toString()
-      },
-    },
-  })
-
-  return output.split('\n').filter((sha) => `${sha}`.trim().length > 0)
-}
-
-async function main() {
-  const shas = await getGitShas()
-  const currentSha = github.context.sha
-
-  // check which image tags exists on s3
-  const existing = new Map()
-  await Promise.all(
-    shas.map(async (sha) => {
-      const Key = [sha, component].join('/')
-      console.log(`Checking s3://${Bucket}/${Key}`)
-
-      try {
-        await s3.headObject({ Bucket, Key }).promise()
-        existing.set(sha, Key)
-      } catch (error) {
-        existing.set(sha, null)
-      }
-    })
-  )
-
-  // Copy existing file
-  const existingSha = shas.find((sha) => existing.get(sha))
-  if (existingSha && existingSha !== currentSha) {
-    const Key = [currentSha, component].join('/')
-    const CopySource = [Bucket, existing.get(existingSha)].join('/')
-    console.log(`Copying s3://${CopySource} => s3://${Bucket}/${Key}`)
-    await s3.copyObject({ Bucket, CopySource, Key }).promise()
-    return Key
-  } else {
-    console.log('No existing configuration found')
-    return null
-  }
-}
-
-main()
-  .then((Key) => {
-    core.setOutput('s3Key', Key)
-  })
-  .catch((error) => {
-    console.error(error)
-    core.error(error.message)
-  })
+const stackName = prNumber ? `pr-${prNumber}` : branch
+core.setOutput('stackName', stackName)
