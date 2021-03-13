79959806d2fd63197dac4313499a7cc58b9d505c
Gudleik Rasch <gudleik@gmail.com>
2021-03-13 15:46:39 +0100

    fix typo

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 const branch =
-  github.context.ref && github.context.ref.match('refs/heads/')
-    ? github.context.ref
-        .replace(/^refs\/heads\//, '')
-        .replace(/\//, '-')
-        .toLowerCase()
-    : null
+  github.context.ref &&
+  github.context.ref.match('refs/heads/') &&
+  github.context.ref
+    .replace(/^refs\/heads\//, '')
+    .replace(/\//, '-')
+    .toLowerCase()
 
-const prNumber = github.context.payload.pull_request?.number
+const prNumber = github.context.payload.pull_request && github.context.payload.pull_request.number
 
 const stackName = prNumber ? `pr-${prNumber}` : branch
 core.setOutput('stackName', stackName)
